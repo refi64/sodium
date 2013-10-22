1e87527fdb4fbb236d4bbb245a83bc939ba273f7
Index Int <vlad.z.4096@gmail.com>
2013-10-22 06:57:17 -0500

    Render using haskell-src-exts
    Ignore-this: 4a0ca240a014d53e4e3f18a4b422e2ba
    
    darcs-hash:20131022115717-ece1f-7c50f65f0d1660a8a88fe4a2c8a6023425175266

--- source/Backend/Render.hs
+++ source/Backend/Render.hs
@@ -4,73 +4,72 @@ module Backend.Render
 	( render
 	) where
 
+import Control.Applicative (empty)
 import Data.List (intersperse)
 import Backend.Program
 
-roundBrace = ("("++) . (++")")
+import qualified Language.Haskell.Exts as E
+
+srcLoc = error "NO SRC LOC"-- E.SrcLoc "" 0 0
 
 render :: Program -> String
-render (Program defs) = unlines $ map renderDef defs
+render (Program defs) = E.prettyPrint md where
+	md = E.Module
+		srcLoc
+		(E.ModuleName "Main")
+		empty
+		empty
+		(Just [E.EVar . E.UnQual . E.name $ "main"])
+		empty
+		decls
+	decls = map renderDef defs
 
 renderDef (ValueDef name expr)
-	= unwords
-	[ renderName name
-	, "="
-	, renderExpression expr
-	]
+	= E.nameBind srcLoc
+		(E.name name)
+		(renderExpression expr)
 
 renderExpression (Access name)
-	= renderName name
+	= E.var (E.name name)
 
 renderExpression (Lambda names expr)
-	= roundBrace
-	$ unwords
-	[ concat 
-		["\\"
-		, unwords $ map renderName names
-		]
-	, "->"
-	, renderExpression expr
-	]
+	= E.lamE srcLoc
+		(map (E.pvar . E.name) names)
+		(renderExpression expr)
 
 renderExpression (Beta expr1 expr2)
-	= roundBrace
-	$ unwords
-	$ map renderExpression
-	$ [expr1, expr2]
+	= E.app
+		(renderExpression expr1)
+		(renderExpression expr2)
 
+-- WARNING: I'm not sure if the pretty-printer
+-- handles fixities right
 renderExpression (Binary op expr1 expr2)
-	= roundBrace
-	$ unwords
-	[ renderExpression expr1
-	, renderName op
-	, renderExpression expr2
-	]
-
-renderExpression (Tuple exprs)
-	= roundBrace
-	$ concat
-	$ intersperse ","
-	$ map renderExpression
-	$ exprs
+	= flip E.infixApp
+		(E.op (E.sym op))
+		(renderExpression expr1)
+		(renderExpression expr2)
 
 renderExpression (Typed expr t)
-	= roundBrace
-	$ unwords
-	[ renderExpression expr
-	, "::"
-	, renderType t
-	]
+	= E.ExpTypeSig srcLoc
+		(renderExpression expr)
+		(renderType t)
 
 renderExpression (Quote cs)
-	= show cs
+	= E.strE cs
 
 renderExpression (Number cs)
-	= cs
+	= E.intE (read cs)
 
-renderName cs
-	= cs
+renderExpression (Tuple [])
+	= E.Con (E.Special E.UnitCon)
+
+renderExpression (Tuple exprs)
+	= E.Tuple (map renderExpression exprs)
 
 renderType = \case
-	HsInteger -> "Integer"
-	HsIO t -> unwords ["IO", renderType t]
+	HsInteger -> E.TyCon (E.UnQual (E.name "Integer"))
+	HsIO t -> E.TyApp
+		(E.TyCon (E.UnQual (E.name "IO")))
+		(renderType t)
+
