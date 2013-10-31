79246a8e4d4c90e7cb74a7f3d523876ce3c9766b
Index Int <vlad.z.4096@gmail.com>
2013-10-31 03:24:52 -0500

    CLI one-line refactoring
    Ignore-this: 93b0d5a977ae09f0797a9d22dc8c53d5
    
    darcs-hash:20131031082452-ece1f-86951744ff7125125085d0634e320c627b392294

--- source/CLI.hs
+++ source/CLI.hs
@@ -19,12 +19,12 @@ main = do
 
 process source = do
 	let tokens = tokenize source
-	print tokens
 	let parsetree = tokens >>= parse
-	print parsetree
 	let modtree = parsetree >>= transform
-	print modtree
 	let dest = render <$> modtree
+	print tokens
+	print parsetree
+	print modtree
 	print dest
 	return $ case dest of
 		Nothing -> "-- CAN\'T TRANSLATE --"
