6f022ea344b979c6e7db7cf9b32fb5bd3ab68d7a
Index Int <vlad.z.4096@gmail.com>
2014-03-03 06:46:59 -0600

    Utilize MultiParamTypeClasses in Dechlorinate
    Ignore-this: f8ba41ff9fc34287f75c9d10caa80a0
    
    darcs-hash:20140303124659-ece1f-5a4f3869697d28de1d8f25fa0c9b77ef99d61bc8

--- source/Backend/Dechlorinate.hs
+++ source/Backend/Dechlorinate.hs
@@ -1,4 +1,10 @@
-{-# LANGUAGE LambdaCase, TupleSections #-}
+{-# LANGUAGE
+   LambdaCase
+ , TupleSections
+ , MultiParamTypeClasses
+ , FunctionalDependencies
+ , FlexibleInstances
+ #-}
 
 module Backend.Dechlorinate
 	( dechlorinate
@@ -14,15 +20,21 @@ import qualified  Backend.Program as D
 import Success
 
 dechlorinate :: S.VecProgram -> (Fail String) D.Program
-dechlorinate (S.VecProgram funcs) = do
-	funcDefs <- mapM dechlorinateFunc funcs
-	return $ D.Program funcDefs
-		[ "Control.Monad"
-		, "Control.Applicative"
-		]
-		[ "LambdaCase"
-		, "TupleSections"
-		]
+dechlorinate = dech
+
+class Dech s d | s -> d where
+	dech :: s -> (Fail String) d
+
+instance Dech S.VecProgram D.Program where
+	dech (S.VecProgram funcs) = do
+		funcDefs <- mapM dech funcs
+		return $ D.Program funcDefs
+			[ "Control.Monad"
+			, "Control.Applicative"
+			]
+			[ "LambdaCase"
+			, "TupleSections"
+			]
 
 data VarState = VarState D.HsType Integer
 type VarStates = M.Map D.Name VarState
@@ -35,260 +47,246 @@ transformName = \case
 			then ("_'"++)
 			else id) cs
 	S.NameUnique name -> transformName name ++ "'_"
-	where
-		reserved
-			= flip elem
-			[ "let"
-			, "show"
-			, "read"
-			, "return"
-			, "foldl"
-			, "map"
-			, "filter"
-			, "undefined"
-			]
+	where reserved = flip elem
+		[ "let"
+		, "show"
+		, "read"
+		, "return"
+		, "foldl"
+		, "map"
+		, "filter"
+		, "undefined"
+		]
 
-dechlorinateName :: S.Name -> Integer -> (Fail String) D.Name
-dechlorinateName name i
-	| i < 0
-		= return
-		$ "const'"
-			++ genericReplicate (pred (-i)) '\''
-			++ transformName name
-	| i > 0
-		= return
-		$ transformName name
-			++ genericReplicate (pred i) '\''
-	| otherwise
-		= return "undefined"
+data Name = Name S.Name Integer
+	deriving (Eq)
 
-dechlorinateType :: S.ClType -> D.HsType
-dechlorinateType = \case
-	S.ClInteger -> D.HsType "Int"
-	S.ClDouble  -> D.HsType "Double"
-	S.ClBoolean -> D.HsType "Bool"
-	S.ClString  -> D.HsType "String"
-	S.ClVoid -> D.HsUnit
+instance Dech Name D.Name where
+	dech (Name name i)
+		| i < 0
+			= return
+			$ "const'"
+				++ genericReplicate (pred (-i)) '\''
+				++ transformName name
+		| i > 0
+			= return
+			$ transformName name
+				++ genericReplicate (pred i) '\''
+		| otherwise
+			= return "undefined"
 
-dechlorinateBody :: S.VecBody -> (Fail String) D.Expression
-dechlorinateBody (S.VecBody _ statements resultExprs) = do
-	hsStatements <- mapM dechlorinateStatement statements
-	hsRetValues <- mapM dechlorinateExpression resultExprs
-	let hsStatement
-		= D.DoExecute
-		$ D.Beta (D.Access "return")
-		$ D.Tuple hsRetValues
-	return $ D.DoExpression (hsStatements ++ [hsStatement])
+instance Dech S.ClType D.HsType where
+	dech = return . \case
+		S.ClInteger -> D.HsType "Int"
+		S.ClDouble  -> D.HsType "Double"
+		S.ClBoolean -> D.HsType "Bool"
+		S.ClString  -> D.HsType "String"
+		S.ClVoid -> D.HsUnit
 
-dechlorinateStatement :: S.VecStatement -> (Fail String) D.DoStatement
-dechlorinateStatement = \case
-	S.VecExecute retIndices (S.ExecuteRead t) [S.VecLValue name i] -> do
-		hsRetPat <- D.PatTuple <$> dechlorinateIndicesList retIndices
-		let hsExpr
-			| t == S.ClString = D.Access "getLine"
-			| otherwise = D.Binary "<$>" (D.Access "read") (D.Access "getLine")
-				`D.Typed` D.HsIO (dechlorinateType t)
-		return $ D.DoBind hsRetPat hsExpr
-	S.VecExecute retIndices S.ExecuteWrite args -> do
-		-- WriteLn can't change its arguments
-		guard $ null retIndices
-		hsArgs <- mapM dechlorinateArgument args
-		return $ case hsArgs of
-			[] -> D.DoExecute $ D.Beta (D.Access "putStrLn") (D.Quote "")
-			hsExprs
-				-> D.DoExecute
-				 $ D.Beta (D.Access "putStrLn")
-				 $ foldr1 (D.Binary "++")
-				 $ hsExprs
-	S.VecAssign name i expr -> do
-		hsExpr <- dechlorinateExpression expr
-		D.DoLet <$> dechlorinateName name i <*> return hsExpr
-	S.VecForStatement retIndices (S.VecForCycle argIndices name exprFrom exprTo clBody) -> do
-		hsRange <- dechlorinateRange exprFrom exprTo
-		hsBody <- dechlorinateBody clBody
+newtype Pure a = Pure a
+
+instance Dech S.VecBody D.Expression where
+	dech (S.VecBody _ statements resultExprs) = do
+		hsStatements <- mapM dech statements
+		hsRetValues <- mapM dech resultExprs
+		let hsStatement
+			= D.DoExecute
+			$ D.Beta (D.Access "return")
+			$ D.Tuple hsRetValues
+		return $ D.DoExpression (hsStatements ++ [hsStatement])
+
+instance Dech S.VecForCycle D.Expression where
+	dech (S.VecForCycle argIndices name exprFrom exprTo clBody) = do
+		hsRange <- dech $ Range exprFrom exprTo
 		hsArgExpr
 			<-  D.Tuple
 			<$> (map D.Access
-			<$> dechlorinateIndicesList argIndices)
-		hsRetPat
-			<-  D.PatTuple
-			<$> dechlorinateIndicesList retIndices
-		hsFoldLambda
-			<-  dechlorinateFoldLambda argIndices name
-			<*> return hsBody
-		return $ D.DoBind
-			hsRetPat
-			(beta
-				[ D.Access "foldM"
-				, hsFoldLambda
-				, hsArgExpr
-				, hsRange
-				]
-			)
-	S.VecIfStatement retIndices (S.VecIfBranch expr bodyThen bodyElse) -> do
-		hsExpr <- dechlorinateExpression expr
-		hsBodyThen <- dechlorinateBody bodyThen
-		hsBodyElse <- dechlorinateBody bodyElse
-		hsRetPat
-			<-  D.PatTuple
-			<$> dechlorinateIndicesList retIndices
-		return $ D.DoBind
-			hsRetPat
-			(D.IfExpression hsExpr hsBodyThen hsBodyElse)
-	_ -> mzero
-
-dechlorinateFunc :: S.VecFunc -> (Fail String) D.Def
-dechlorinateFunc (S.VecFunc (S.FuncSig S.NameMain params S.ClVoid) clBody)
-	= do
-		guard $ M.null params
-		hsBody <- dechlorinateBody clBody
-		return $ D.ValueDef (D.PatFunc "main" []) hsBody
-dechlorinateFunc (S.VecFunc (S.FuncSig name params retType) clBody)
-	 =  D.ValueDef (D.PatFunc (transformName name) paramNames)
-	<$> dechlorinatePureBody clBody
-	where
-		paramNames
-			= map transformName
-			$ M.keys params
-
-dechlorinateRange :: S.VecExpression -> S.VecExpression -> (Fail String) D.Expression
-dechlorinateRange exprFrom exprTo
-	 =  D.Range
-	<$> dechlorinateExpression exprFrom
-	<*> dechlorinateExpression exprTo
-
-dechlorinateFoldLambda :: S.IndicesList -> S.Name -> (Fail String) (D.Expression -> D.Expression)
-dechlorinateFoldLambda indices name = do
-	hsNames <- dechlorinateIndicesList indices
-	hsName <- dechlorinateName name (-1)
-	return $ D.Lambda
-		[ D.PatTuple hsNames
-		, D.PatTuple [hsName]
-		]
-
-dechlorinatePureBody
-	:: S.VecBody
-	-> (Fail String) D.Expression
-dechlorinatePureBody (S.VecBody _ statements resultExprs) = msum
-	[ do
-		name1 <- case resultExprs of
-			[S.VecAccess name i] -> return (name, i)
-			_ -> mzero
-		let appToLast f xs = case reverse xs of
-			(x:xs') -> (, reverse xs') <$> f x
-			_ -> mzero
-		((name2, hsExpr), statements)
-			<- flip appToLast statements
-			$ \case
-				S.VecAssign name i expr -> do
-					hsExpr <- dechlorinateExpression expr
-					return ((name, i), hsExpr)
-				S.VecCaseStatement [(name, i)] vecCaseBranch -> do
-					hsExpr <- dechlorinatePureVecCaseBranch vecCaseBranch
-					return ((name, i), hsExpr)
-				_ -> mzero
-		guard $ name1 == name2
-		hsValueDefs <- mapM dechlorinatePureStatement statements
-		return $ pureLet hsValueDefs hsExpr
-	, do
-		hsValueDefs <- mapM dechlorinatePureStatement statements
-		hsRetValues <- mapM dechlorinateExpression resultExprs
-		return $ pureLet hsValueDefs (D.Tuple hsRetValues)
-	]
-
-dechlorinatePureVecCaseBranch (S.VecCaseBranch expr leafs bodyElse) = do
-	(caseExpr, wrap) <- case expr of
-		S.VecAccess name i -> do
-			hsName <- dechlorinateName name i
-			return (D.Access hsName, id)
-		expr -> do
-			hsExpr <- dechlorinateExpression expr
-			return
-				( D.Access "__CASE__"
-				, pureLet [D.ValueDef (D.PatTuple ["__CASE__"]) hsExpr]
-				)
-	let dechlorinateLeaf (exprs, body) = do
-		let genGuard = \case
-			S.VecCall (S.CallOperator S.OpRange) [exprFrom, exprTo] -> do
-				hsRange <- dechlorinateRange exprFrom exprTo
-				return $ D.Binary "`elem`" caseExpr hsRange
-			expr -> do
-				hsExpr <- dechlorinateExpression expr
-				return $ D.Binary "==" caseExpr hsExpr
-		hsExprs <- mapM genGuard exprs
-		hsBody <- dechlorinatePureBody body
-		return (foldl1 (D.Binary "||") hsExprs, hsBody)
-	hsBodyElse <- dechlorinatePureBody bodyElse
-	hsGuardLeafs
-		<- (++ [(D.Access "otherwise", hsBodyElse)])
-		<$> mapM dechlorinateLeaf leafs
-	let hsGuardExpression name
-		= pureLet [D.GuardDef (D.PatTuple [name]) hsGuardLeafs]
-		$ D.Access name
-	return $ wrap (hsGuardExpression "__RES__")
-
-dechlorinatePureVecIfBranch (S.VecIfBranch expr bodyThen bodyElse)
-	 =  D.IfExpression
-	<$> dechlorinateExpression expr
-	<*> dechlorinatePureBody bodyThen
-	<*> dechlorinatePureBody bodyElse
-
-dechlorinatePureVecForCycle (S.VecForCycle argIndices name exprFrom exprTo clBody) = do
-		hsRange <- dechlorinateRange exprFrom exprTo
-		hsBody <- dechlorinatePureBody clBody
-		hsArgExpr
-			<- D.Tuple
-			<$> (map D.Access
-				<$> dechlorinateIndicesList argIndices)
-		hsFoldLambda <- dechlorinateFoldLambda argIndices name <*> return hsBody
+			<$> dech (IndicesList argIndices))
+		hsFoldLambda <- dech (FoldLambda argIndices name) <*> dech clBody
 		return $ beta
-			[ D.Access "foldl"
+			[ D.Access "foldM"
 			, hsFoldLambda
 			, hsArgExpr
 			, hsRange
 			]
 
-dechlorinatePureStatement
-	:: S.VecStatement
-	-> (Fail String) D.ValueDef
-dechlorinatePureStatement = \case
-	S.VecAssign name i expr -> do
-		hsExpr <- dechlorinateExpression expr
-		hsName <- dechlorinateName name i
-		return $ D.ValueDef (D.PatFunc hsName []) hsExpr
-	S.VecForStatement retIndices vecForCycle
-		 -> D.ValueDef
-		<$> (D.PatTuple
-			<$> dechlorinateIndicesList retIndices)
-		<*> dechlorinatePureVecForCycle vecForCycle
-	S.VecIfStatement retIndices vecIfBranch
-		 -> D.ValueDef
-		<$> (D.PatTuple
-			<$> dechlorinateIndicesList retIndices)
-		<*> dechlorinatePureVecIfBranch vecIfBranch
-	S.VecCaseStatement retIndices vecCaseBranch
-		 -> D.ValueDef
-		<$> (D.PatTuple
-			<$> dechlorinateIndicesList retIndices)
-		<*> dechlorinatePureVecCaseBranch vecCaseBranch
-	_ -> mzero
+instance Dech S.VecIfBranch D.Expression where
+	dech (S.VecIfBranch expr bodyThen bodyElse) = do
+		hsExpr <- dech expr
+		hsBodyThen <- dech bodyThen
+		hsBodyElse <- dech bodyElse
+		return $ D.IfExpression hsExpr hsBodyThen hsBodyElse
+
+instance Dech S.VecStatement D.DoStatement where
+	dech = \case
+		S.VecExecute retIndices (S.ExecuteRead t) [S.VecLValue name i] -> do
+			hsRetPat <- D.PatTuple <$> dech (IndicesList retIndices)
+			hsExpr <- if t == S.ClString
+				then return $ D.Access "getLine"
+				else do
+					hsType <- dech t
+					return
+						$ D.Binary "<$>" (D.Access "read") (D.Access "getLine")
+						`D.Typed` D.HsIO hsType 
+			return $ D.DoBind hsRetPat hsExpr
+		S.VecExecute retIndices S.ExecuteWrite args -> do
+			-- WriteLn can't change its arguments
+			guard $ null retIndices
+			hsArgs <- mapM dech args
+			return $ case hsArgs of
+				[] -> D.DoExecute $ D.Beta (D.Access "putStrLn") (D.Quote "")
+				hsExprs
+					-> D.DoExecute
+					 $ D.Beta (D.Access "putStrLn")
+					 $ foldr1 (D.Binary "++")
+					 $ hsExprs
+		S.VecAssign name i expr -> do
+			hsExpr <- dech expr
+			D.DoLet <$> dech (Name name i) <*> return hsExpr
+		S.VecForStatement retIndices vecForCycle
+			-> wrap retIndices vecForCycle
+		S.VecIfStatement retIndices vecIfBranch
+			-> wrap retIndices vecIfBranch
+		_ -> mzero
+		where wrap retIndices vecPart = do
+			hsRetPat <- D.PatTuple <$> dech (IndicesList retIndices)
+			D.DoBind hsRetPat <$> dech vecPart
+
+instance Dech S.VecFunc D.Def where
+	dech (S.VecFunc (S.FuncSig S.NameMain params S.ClVoid) clBody) = do
+		guard $ M.null params
+		hsBody <- dech clBody
+		return $ D.ValueDef (D.PatFunc "main" []) hsBody
+	dech (S.VecFunc (S.FuncSig name params retType) clBody)
+		 =  D.ValueDef (D.PatFunc (transformName name) paramNames)
+		<$> dech (Pure clBody)
+		where paramNames = map transformName (M.keys params)
+
+data Range = Range S.VecExpression S.VecExpression
+
+instance Dech Range D.Expression where
+	dech (Range exprFrom exprTo)
+		 =  D.Range
+		<$> dech exprFrom
+		<*> dech exprTo
+
+data FoldLambda = FoldLambda S.IndicesList S.Name
+
+instance Dech FoldLambda (D.Expression -> D.Expression) where
+	dech (FoldLambda indices name) = do
+		hsNames <- dech (IndicesList indices)
+		hsName <- dech (Name name (-1))
+		return $ D.Lambda [D.PatTuple hsNames, D.PatTuple [hsName]]
+
+instance Dech (Pure S.VecBody) D.Expression where
+	dech (Pure (S.VecBody _ statements resultExprs)) = msum
+		[ do
+			name1 <- case resultExprs of
+				[S.VecAccess name i] -> return $ Name name i
+				_ -> mzero
+			let appToLast f xs = case reverse xs of
+				(x:xs') -> (, reverse xs') <$> f x
+				_ -> mzero
+			let dechStatement = \case
+				S.VecAssign name i expr
+					-> (Name name i, ) <$> dech expr
+				S.VecCaseStatement [(name, i)] vecCaseBranch
+					-> (Name name i, ) <$> dech (Pure vecCaseBranch)
+				_ -> mzero
+			((name2, hsExpr), statements) <- appToLast dechStatement statements
+			guard $ name1 == name2
+			hsValueDefs <- mapM dech (map Pure statements)
+			return $ pureLet hsValueDefs hsExpr
+		, do
+			hsValueDefs <- mapM dech (map Pure statements)
+			hsRetValues <- mapM dech resultExprs
+			return $ pureLet hsValueDefs (D.Tuple hsRetValues)
+		]
+
+instance Dech (Pure S.VecCaseBranch) D.Expression where
+	dech (Pure (S.VecCaseBranch expr leafs bodyElse)) = do
+		(caseExpr, wrap) <- case expr of
+			S.VecAccess name i -> do
+				hsName <- dech (Name name i)
+				return (D.Access hsName, id)
+			expr -> do
+				hsExpr <- dech expr
+				let wrap = pureLet [D.ValueDef (D.PatTuple ["__CASE__"]) hsExpr]
+				return (D.Access "__CASE__", wrap)
+		let genGuard = \case
+			S.VecCall (S.CallOperator S.OpRange) [exprFrom, exprTo] -> do
+				hsRange <- dech $ Range exprFrom exprTo
+				return $ D.Binary "`elem`" caseExpr hsRange
+			expr -> do
+				hsExpr <- dech expr
+				return $ D.Binary "==" caseExpr hsExpr
+		let dechLeaf (exprs, body) = do
+			hsExprs <- mapM genGuard exprs
+			hsBody <- dech (Pure body)
+			return (foldl1 (D.Binary "||") hsExprs, hsBody)
+		hsBodyElse <- dech (Pure bodyElse)
+		hsGuardLeafs
+			<- (++ [(D.Access "otherwise", hsBodyElse)])
+			<$> mapM dechLeaf leafs
+		let hsGuardExpression name
+			= pureLet [D.GuardDef (D.PatTuple [name]) hsGuardLeafs]
+			$ D.Access name
+		return $ wrap (hsGuardExpression "__RES__")
+
+instance Dech (Pure S.VecIfBranch) D.Expression where
+	dech (Pure (S.VecIfBranch expr bodyThen bodyElse))
+		 =  D.IfExpression
+		<$> dech expr
+		<*> dech (Pure bodyThen)
+		<*> dech (Pure bodyElse)
+
+instance Dech (Pure S.VecForCycle) D.Expression where
+	dech (Pure (S.VecForCycle argIndices name exprFrom exprTo clBody)) = do
+		hsRange <- dech $ Range exprFrom exprTo
+		hsArgExpr
+			<- D.Tuple
+			<$> (map D.Access
+				<$> dech (IndicesList argIndices))
+		hsFoldLambda
+			<-  dech (FoldLambda argIndices name)
+			<*> dech (Pure clBody)
+		return $ beta [D.Access "foldl", hsFoldLambda, hsArgExpr, hsRange]
+
+instance Dech (Pure S.VecStatement) D.ValueDef where
+	dech (Pure statement) = case statement of
+		S.VecAssign name i expr -> do
+			hsExpr <- dech expr
+			hsName <- dech (Name name i)
+			return $ D.ValueDef (D.PatFunc hsName []) hsExpr
+		S.VecForStatement retIndices vecForCycle
+			-> wrap retIndices vecForCycle
+		S.VecIfStatement retIndices vecIfBranch
+			-> wrap retIndices vecIfBranch
+		S.VecCaseStatement retIndices vecCaseBranch
+			-> wrap retIndices vecCaseBranch
+		_ -> mzero
+		where wrap retIndices vecPart
+			 = D.ValueDef
+			<$> (D.PatTuple <$> dech (IndicesList retIndices))
+			<*> dech (Pure vecPart)
 
 beta = foldl1 D.Beta
 
-pureLet [] expr = expr
-pureLet defs expr = D.PureLet defs expr
+pureLet [] = id
+pureLet defs = D.PureLet defs
 
-dechlorinateIndicesList
-	= mapM (uncurry dechlorinateName)
+newtype IndicesList = IndicesList S.IndicesList
 
-dechlorinateArgument :: S.VecArgument -> (Fail String) D.Expression
-dechlorinateArgument = \case
-	S.VecLValue name i -> D.Access <$> dechlorinateName name i
-	S.VecRValue expr -> dechlorinateExpression expr
+instance Dech IndicesList [D.Name] where
+	dech (IndicesList xs) = mapM (dech . uncurry Name) xs
 
-dechlorinateExpression :: S.VecExpression -> (Fail String) D.Expression
-dechlorinateExpression = \case
-	S.VecPrimary prim -> return $ case prim of
+instance Dech S.VecArgument D.Expression where
+	dech = \case
+		S.VecLValue name i -> D.Access <$> dech (Name name i)
+		S.VecRValue expr -> dech expr
+
+instance Dech S.VecExpression D.Expression where
+	dech (S.VecPrimary prim) = return $ case prim of
 		S.Quote  cs -> D.Quote cs
 		S.INumber intSection -> D.INumber intSection
 		S.FNumber intSection fracSection
@@ -298,26 +296,24 @@ dechlorinateExpression = \case
 		S.BTrue  -> D.BTrue
 		S.BFalse -> D.BFalse
 		S.Void   -> D.Tuple []
-	S.VecAccess name i -> D.Access <$> dechlorinateName name i
-	S.VecCall callName exprs -> do
-		hsExprs <- mapM dechlorinateExpression exprs
+	dech (S.VecAccess name i) = D.Access <$> dech (Name name i)
+	dech (S.VecCall callName exprs) = do
+		hsExprs <- mapM dech exprs
 		let binary hsOp = case hsExprs of
 			hsExpr1:hsExpr2:hsExprs ->
 				return $ beta (D.Binary hsOp hsExpr1 hsExpr2 : hsExprs)
+			_ -> mzero
+		let unary hsOp = case hsExprs of
+			hsExpr1:hsExprs ->
+				return $ beta (hsOp hsExpr1 : hsExprs)
 			_ -> mzero
 		case callName of
 			S.CallName name
 				-> return
 				 $ beta (D.Access (transformName name) : hsExprs)
 			S.CallOperator op -> case op of
-				S.OpNegate -> do
-					case hsExprs of
-						[hsExpr1] -> return (D.Negate hsExpr1)
-						_ -> mzero
-				S.OpShow -> do
-					case hsExprs of
-						[hsExpr1] -> return (D.Access "show" `D.Beta` hsExpr1)
-						_ -> mzero
+				S.OpNegate -> unary D.Negate
+				S.OpShow -> unary (D.Beta $ D.Access "show")
 				S.OpAdd -> binary "+"
 				S.OpSubtract -> binary "-"
 				S.OpMultiply -> binary "*"
